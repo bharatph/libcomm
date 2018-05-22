2ec5adcd468e9b66f5c26b94f0825dd26422aeed
Bharatvaj <bharatvajph@gmail.com>
2018-05-22 12:08:17 +0530

    Changed library name to comm, breaks API from this commit due to various function name change

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# net_util
-Cross-Platform network library for C
+# libcomm
+Cross-Platform TCP/IPv$ library for C for reading and writing to sockets
+libcomm is a wrapper library for Linux, Unix and Windows for Communication using TCP/IPv4
+
+### Why?
+Because Winsocks API is stupid and no one deserves to suffer from it
---
+++ include/comm.h
@@ -0,0 +1,78 @@
+#ifndef _COMM
+#define _COMM "COMM"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__linux__) || defined(__APPLE__)
+#if defined(__linux__) && defined(kernel_version_2_4)
+#include <sys/sendfile.h>
+#endif
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+typedef int SOCKET;
+#elif _WIN32
+#include <windows.h>
+#include <ws2tcpip.h>
+#include<winsock2.h>
+#else
+#error OS not supported
+#endif
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 256
+#endif
+
+#ifndef CON_MAX_ATTEMPTS
+#define CON_MAX_ATTEMPTS 5
+#endif
+
+#ifndef SERV_BACKLOG
+#define SERV_BACKLOG 10
+#endif
+
+/*
+ * Write data to a given socket
+ * @param sockfd The socket descriptor to write to
+ * @param buffer The message to be written to the socket
+ * @return Success value is returned mentioning a value is sent or not
+ * 0 for success
+ * 1 for failure
+ */
+
+int comm_write_data(SOCKET sockfd, void *buffer);
+
+/*
+ * Read data from a socket
+ * @param sockfd The socket descriptor to write to
+ * @return buffer data from the socket
+ */
+
+void *comm_read_data(SOCKET sockfd);
+
+/*
+ * Disconnects from the server
+ * @param sockfd The socket to be closed
+ */
+int comm_disconnect_server(SOCKET sockfd);
+
+/** Connects to the server with the standard IPv4 and TCP stack
+ * @param hostname IPv4 address or hostname
+ * @param port Port number for the server to start
+ * @return Socket descriptor of the started server
+ */
+int comm_connect_server(const char *hostname, int port);
+
+//TODO support multiple server options
+/** Starts the server with the standard IPv4 and TCP stack
+ * @param port Port number for the server to start
+ * @return Socket descriptor of the started server
+ */
+int comm_start_server(SOCKET port);
+
+#endif
